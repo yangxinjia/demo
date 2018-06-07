26f7deb109df1ae6001dfe0c2dd06dc2e0f340ee
yangxinjia <xinjiayang@deepglint.com>
2018-06-07 20:11:38 +0800

    deploy online

--- deploy_image.sh
+++ deploy_image.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/usr/bin/expect              
 version=$(cat VERSION)
-cp $(pwd)/release/images/demo-$version.tar.gz /root/work/
-cd /root/work/
-tar -zxvf demo-$version.tar.gz
-cd demo-$version
-./run_image.sh
+set timeout 3
+spawn scp ./release/images/demo-$version.tar.gz root@172.24.156.85:~/work/
+expect "password"
+send "L70aSp12\r"
+interact
+
+set timeout 3
+spawn ssh root@172.24.156.85 "cd work; tar -zxvf demo-$version.tar.gz; cd demo-$version; ./run_image.sh"
+expect "password"
+send "L70aSp12\r"
+interact
