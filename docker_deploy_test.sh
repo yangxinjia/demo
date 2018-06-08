15af7c797e1b8929e8131fcf3ce412ae25378ff0
yangxinia <xinjiayang@deepglint.com>
2018-06-08 16:58:29 +0800

    modify

--- docker_deploy_test.sh
+++ docker_deploy_test.sh
@@ -5,4 +5,10 @@ docker run --rm -i \
 -v $(pwd):/demo/ \
 -w /demo \
 docker.dg-atlas.com:5000/demo-test:1.0 \
-./deploy.sh && python function_test.py
+./deploy.sh
+
+docker run --rm -i \
+-v $(pwd):/demo/ \
+-w /demo \
+docker.dg-atlas.com:5000/demo-test:1.0 \
+python function_test.py
