cf8c21735f7c53e780b9743f4d58121d034784a0
yangxinia <xinjiayang@deepglint.com>
2018-06-08 18:50:25 +0800

    pressure test

--- docker_deploy_test.sh
+++ docker_deploy_test.sh
@@ -12,3 +12,10 @@ docker run --rm -i \
 -w /demo \
 docker.dg-atlas.com:5000/demo-test:1.0 \
 python function_test.py
+
+docker run --rm -i \
+-v $(pwd):/demo/ \
+-w /demo \
+docker.dg-atlas.com:5000/demo-test:1.0 \
+python pressure.py
+
