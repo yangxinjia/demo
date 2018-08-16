06598217702a3a0de8e6a8f8c5c150b054512f69
yangxinjia <xinjiayang@deepglint.com>
2018-08-16 12:33:49 +0800

    test27

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ function compile(){
 }
 function test(){
     mkdir -p report
-    nosetests demo_test.py --with-xunit --xunit-file=./report/result.html
+    nosetests demo_test.py --with-xunit --xunit-file=./report/result.xml
     exit
 }
 function run(){
